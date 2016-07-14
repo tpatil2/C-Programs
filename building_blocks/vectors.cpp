0e9705b433b5b71f722d5743117abe5da3a41394
Tejas Patil <tpatil2@mail.csuchico.edu>
2016-07-13 23:39:39 -0700

    vector implementaions

---
+++ building_blocks/vectors.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+  //vectors are like arrays but size is changed
+  //decleration  vector <datatype> vectorname(size);
+
+    vector<int> myvector(10);
+
+    
+
+
+
+  return 0;
+}
