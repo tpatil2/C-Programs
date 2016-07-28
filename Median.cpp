734b310d55926fb265ee63ccda70e3894b0290b8
Tejas Patil <tpatil2@mail.csuchico.edu>
2016-07-28 00:43:23 -0700

    Median of 3 Num

---
+++ Median.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+int main(){
+
+    char arr[] ="tejad \n ok";
+
+    int A=9,B=33,C=11;
+
+    int median;
+
+    if ((A - B) * (C - B) <= 0) {
+       median=B;
+   } else if ((B - C) * (A - C) <= 0) {
+       median=C;
+   } else {
+       median=A;
+   }
+
+    std::cout << "median is "<< median << std::endl;
+
+  return 0;
+}
