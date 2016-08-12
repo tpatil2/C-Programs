ddfc5ce79aa7d0a709aa5cadd4618e3905ad7619
tpatil2 <tpatil2@mail.csuchico.edu>
2016-08-11 23:31:04 -0700

    find all palindrome substrings

--- Strings/Longestsubstring.cpp
+++ Strings/Longestsubstring.cpp
@@ -3,17 +3,34 @@
 
 using namespace std;
 
+bool Is_palindrome(string str){
+
+  int i=0;
+  size_t j=str.length()-1;
+
+  while(i<j){
+    if(str[i]==str[j]){
+      i++;
+      j--;
+    }else return false;
+    }
+
+return true;
+}
+
 
 int main(){
 
-  string str ="abcdefghijklmnopqrstuvwxyz";
+  string str ="tejanitinPatil";
 
-  for(int i=str.length();i>1;i--){
+  for(int i=str.length();i>0;i--){
     for(int j=0;j+i<=str.length();j++){
-      std::cout << str.substr(j,i) << std::endl;
+      //std::cout << str.substr(j,i) << std::endl;
+      if(Is_palindrome(str.substr(j,i))){
+        std::cout << str.substr(j,i)<<" Is palindrome" << std::endl;
+      }
     }
   }
 
   return 0;
-
 }
